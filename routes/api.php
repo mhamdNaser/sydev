a1e05834f557c8f7418643c9ffa7eb23cc92b35d
mhamdNaser <mhamd.naser.edden@gmail.com>
2025-10-07 12:21:56 +0300

    register

--- routes/api.php
+++ routes/api.php
@@ -10,10 +10,14 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::post('admin/login', [AuthController::class, 'adminLogin']);
 
-Route::prefix( 'admin' )->group(function(){
-    Route::get('alladmin', [ UserController::class, 'index' ])->name('alladmin');
+Route::prefix('admin')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('admin/login', [AuthController::class, 'adminLogin']);
+    Route::get('alladmin', [UserController::class, 'index'])->name('alladmin');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
