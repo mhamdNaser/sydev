6736ddfd59c77488a563741b11a61f303b19f3b1
mhamdNaser <mhamd.naser.edden@gmail.com>
2025-10-12 16:06:28 +0300

    image convert

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -37,11 +37,13 @@ class ImageController extends Controller
         $manager = new ImageManager($driver);
 
         try {
-            // قراءة الصورة باستخدام read() بدل make()
+            // قراءة الصورة
             $image = $manager->read($imageFile->getPathname());
 
+            // إعادة تسمية الملف لتجنب المسافات والرموز الخاصة
             $originalName = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-            $fileName = $originalName . '.' . $format;
+            $safeName = preg_replace('/[^A-Za-z0-9_\-]/', '_', $originalName); // استبدل الرموز بـ _
+            $fileName = $safeName . '.' . $format;
 
             $savePath = 'images/converted/' . $fileName;
             Storage::disk('public')->makeDirectory('images/converted');
@@ -63,9 +65,12 @@ class ImageController extends Controller
                     break;
             }
 
+            // تشفير URL لتجنب مشاكل المسافات والرموز
+            $encodedPath = implode('/', array_map('rawurlencode', explode('/', $savePath)));
+
             return response()->json([
                 'success' => true,
-                'url' => asset('storage/' . $savePath)
+                'url' => asset('storage/' . $encodedPath)
             ]);
         } catch (\Exception $e) {
             return response()->json([
