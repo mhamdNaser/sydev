b96254a1b09d13d00db1278a75eb0e7966cf439e
mhamdNaser <mhamd.naser.edden@gmail.com>
2025-10-16 02:05:52 +0300

    image convert

--- app/Http/Controllers/Api/IconController.php
+++ app/Http/Controllers/Api/IconController.php
@@ -41,7 +41,7 @@ class IconController extends Controller
 
     public function download($fileName)
     {
-        $path = public_path('images/' . $fileName);
+        $path = public_path('images/icons/' . $fileName);
 
         if (!file_exists($path)) {
             return response()->json([
