53c2c43d146944cc325c25891f4f38d6c7a2aaa3
mhamdNaser <mhamd.naser.edden@gmail.com>
2025-10-22 13:48:56 +0300

    download count

---
+++ app/Http/Controllers/Api/IconCssController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+
+class IconCssController extends Controller
+{
+    public function generate()
+    {
+        // استخدم الكاش لتخفيف الضغط على السيرفر
+        $css = Cache::remember('icons_css', 60 * 60, function () {
+            $icons = DB::table('icons')->get();
+            $css = "";
+
+            foreach ($icons as $icon) {
+                $svgPath = public_path($icon->url);
+                if (!file_exists($svgPath)) continue;
+
+                $svgContent = file_get_contents($svgPath);
+                $svgDataUri = 'data:image/svg+xml;base64,' . base64_encode($svgContent);
+
+                $css .= ".sydev-{$icon->name} {
+                    background: url('{$svgDataUri}') no-repeat center;
+                    background-size: contain;
+                    display: inline-block;
+                    width: 1em;
+                    height: 1em;
+                }\n";
+            }
+
+            $css .= ".sydev { display:inline-block; vertical-align:middle; }";
+            return $css;
+        });
+
+        return response($css, 200, ['Content-Type' => 'text/css']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\IconCssController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/icons.css', [IconCssController::class, 'generate']);
